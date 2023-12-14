3d5c4a15538fe5ac1b1691ebcec56645d054d7cc
mbanya <mbanyaw@gmail.com>
2023-12-14 17:48:16 +0300

    First Draft of Copy on Index,Services & About Us Pages

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,7 +30,26 @@
     <link rel="stylesheet" href="{{asset('main/css/flaticon.css')}}">
     <link rel="stylesheet" href="{{asset('main/css/style.css')}}">
     <link rel="stylesheet" href="{{asset('main/css/responsive.css')}}">
+    <!-- Microsoft Clarity -->
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "k5txr9bryx");
+    </script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QB6RCL64KD"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-QB6RCL64KD');
+    </script>
     @yield('assets')
+
 </head>
 <body>
 @yield('content')
